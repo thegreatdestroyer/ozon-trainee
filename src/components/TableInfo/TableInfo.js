6dbe327f4d864451f5e8bff2e69943fe73f07811
asambur <asambur@ozon.ru>
2021-11-17 14:49:04 +0300

    Добавил стили для карточки товара, возврат на таблицу с товарами

--- src/components/TableInfo/TableInfo.js
+++ src/components/TableInfo/TableInfo.js
@@ -2,18 +2,28 @@ import React from 'react';
 import './TableInfo.css';
 import { setCardItemAction } from '../../store/TableInfo/actions';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router';
 
 const TableInfo = ({ item }) => {
 
+  const history = useHistory();
+
+  const handleGoToTable = () => {
+    history.push("/table");
+  }
+
     return (
-       <div>
-        <h2>{item.name}</h2>
-        <div>{item.artikul}</div>
-        <div>{item.type}</div>
-        <div>{item.date}</div>
-        <div>{item.price}</div>
-        <div>{item.quantity}</div>
+      <div>
+        <div className="card-container">
+          <h2>{item.name}</h2>
+          <div>Артикул: {item.artikul}</div>
+          <div>Тип: {item.type}</div>
+          <div>Дата: {item.date}</div>
+          <div>Цена: {item.price}</div>
+          <div>Количество: {item.quantity}</div>
         </div>
+        <button className="button" onClick={handleGoToTable}>Назад</button>
+      </div>
     );
 }
 
